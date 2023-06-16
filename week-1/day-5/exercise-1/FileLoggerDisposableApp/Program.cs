b73447676ace32b341bbc4b20a25d6228f664f90
devanshmehta04 <devanshmehta72@gmail.com>
2023-06-16 12:52:07 +0530

    COMMIT ALL

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -5,8 +5,18 @@
         static void Main(string[] args)
         {
             // Use FileLogger and dispose of it properly
+            string[] lines = { "1", "Se", "jikji" };
+            // Set a variable to the Documents path.
+            string docPath = "C:/Users/admin/Desktop/devansh/4 week/week-1/day-5/exercise-1/FileLoggerDisposableApp";
+            // Write the string array to a new file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "myfile.txt")))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            }
         }
     }
+}
 
     class FileLogger : IDisposable
     {
@@ -14,17 +24,31 @@
 
         public FileLogger(string filePath)
         {
-            // Initialize StreamWriter instance
-        }
+        // Initialize StreamWriter instance
+        _writer = new StreamWriter(filePath);
+    }
 
         public void Dispose()
         {
-            // Implement IDisposable pattern
+        // Implement IDisposable pattern
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+    protected virtual void Dispose(bool disposing)
+    {
+        {
+            if (disposing)
+            {
+                _writer.Dispose();
+                _writer = null;
+            }
         }
+    }
 
         public void Log(string message)
         {
-            // Write message to the file
-        }
-    }
+    // Write message to the file
+    _writer.WriteLine(message);
+         }
+   
 }
